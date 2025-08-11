459f599b1c9780e5e8703f7016b8819f3a704282
AdityaS-2010 <adi4utube@gmail.com>
2025-08-10 21:16:02 -0700

    CORS

--- scripts/server.js
+++ scripts/server.js
@@ -9,7 +9,9 @@ const ASSISTANT_API_ID = process.env.ASSISTANT_API_ID;
 app.options('*', cors());
 const allowedOrigins = [
   'http://localhost:5500',
-  'http://127.0.0.1:5500'
+  'http://127.0.0.1:5500',
+  'https://adityas-2010.github.io',
+  'https://adityas-2010.github.io/CPFD_2025'
 ];
 
 app.use(cors({
